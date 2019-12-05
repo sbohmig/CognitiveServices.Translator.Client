c0f71948deec8dd1f28c517142545e1ef03dc5e1
sbohmig <58512481+sbohmig@users.noreply.github.com>
2019-12-05 09:13:52 +0100

    Make IDisposable to handle explicit disposal of HttpClient instance. Permit to pass optional proxy instance.

--- CognitiveServices.Translator.Client/LanguageClient.cs
+++ CognitiveServices.Translator.Client/LanguageClient.cs
@@ -1,132 +1,152 @@
-﻿using CognitiveServices.Translator.Client.Extension;
-using CognitiveServices.Translator.Client.Language;
-using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using CognitiveServices.Translator.Configuration;
+using CognitiveServices.Translator.Translate;
+using Newtonsoft.Json;
 
-namespace CognitiveServices.Translator.Client
+namespace CognitiveServices.Translator
 {
-    /// <summary>
-    /// Get the languages supported within the system.
-    /// </summary>
-    /// <seealso cref="ILanguageClient" />
-    /// <remarks>
-    /// The API's accessed within this client does not require the API Key. 
-    /// </remarks>
-    public class LanguageClient : ILanguageClient
-    {
-        private const string UriExtensionPath = "languages";
-        private readonly HttpClient _httpClient;
-        private string _headerETag = null;
-        private ResponseBody _cachedFullScopes;
+	/// <summary>
+	/// Translate using cognitive service from Microsoft
+	/// </summary>
+	/// <seealso cref="ITranslateClient" />
+	/// <remarks>https://docs.microsoft.com/en-us/azure/cognitive-services/translator/quickstarts/csharp</remarks>
+	public class TranslateClient : ITranslateClient, IDisposable
+	{
+		private const string UriExtensionPath = "translate";
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="TranslateClient" /> class.
-        /// </summary>
-        /// <param name="logger">The logger.</param>
-        /// <param name="cognitiveServiceConfig">The cognitive service configuration.</param>
-        public LanguageClient()
-        {
-            // In Core 2.1, we can use what is described in: https://docs.microsoft.com/en-gb/aspnet/core/fundamentals/http-requests?view=aspnetcore-2.1
-            // Otherwise, maybe use RestSharp?
-            _httpClient = new HttpClient
-            {
-                BaseAddress = new Uri(Constants.TranslatorApiBasePath)
-            };
-        }
+		private readonly CognitiveServicesConfig _cognitiveServiceConfig = new CognitiveServicesConfig();
+		private HttpClient _httpClient;
 
-        /// <summary>
-        /// Gets the specified scope.
-        /// </summary>
-        /// <param name="settings">The settings.</param>
-        /// <returns></returns>
-        /// <remarks>
-        /// Default scope is ALL scope.
-        /// </remarks>
-        public Task<ResponseBody> GetAsync(RequestHeader settings)
-        {
-            return GetAsync(settings, Scope.All);
-        }
 
-        /// <summary>
-        /// Gets the specified scope.
-        /// </summary>
-        /// <param name="settings">The settings.</param>
-        /// <param name="scopes">The scopes.</param>
-        /// <returns></returns>
-        public async Task<ResponseBody> GetAsync(RequestHeader settings, Scope scopes)
-        {
-            if (settings == null) throw new ArgumentNullException(nameof(settings));
 
-            var scopeFlags = string.Join(",", scopes.GetFlags());
-            var qs = $"api-version={Constants.ApiVersion}&scope={scopeFlags.Replace(",All", "")}";
+		public const int MaxNumberOfRequestContent = 25;
+		public const int MaxNumberOfCharacterPerRequest = 5_000;
 
-            // Not the best, but for now it will do.
-            if (_cachedFullScopes != null)
-                return _cachedFullScopes; // TODO use a memory cache, or other cache.
 
-            using (var request = new HttpRequestMessage(HttpMethod.Get, $"{UriExtensionPath}?{qs}"))
-            {
-                request.Headers.AcceptLanguage.TryParseAdd(settings.AcceptLanguage.Name);
-                if (settings.ClientTraceId.HasValue)
-                    request.Headers.Add("X-ClientTraceId", settings.ClientTraceId.Value.ToString());
 
-                using (var response = await _httpClient.SendAsync(request).ConfigureAwait(false))
-                {
-                    if (response.IsSuccessStatusCode)
-                    {
-                        response.Headers.TryGetValues("ETag", out var etag);
-                        response.Headers.TryGetValues("X-RequestId", out var xRequestId);
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TranslateClient" /> class.
+		/// </summary>
+		/// <param name="logger">The logger.</param>
+		/// <param name="cognitiveServiceConfig">The cognitive service configuration.</param>
+		public TranslateClient(CognitiveServicesConfig cognitiveServiceConfig, IWebProxy proxy = null)
+		{
+			_cognitiveServiceConfig = cognitiveServiceConfig;
 
-                        var responseBody = await response.Content.ReadAsStringAsync();
-                        var result = JsonConvert.DeserializeObject<ResponseBody>(responseBody);
 
-                        if (scopes == Scope.All)
-                            _cachedFullScopes = result;
+			proxy = proxy ?? WebRequest.DefaultWebProxy;
+			var handler = new HttpClientHandler()
+			{
+				Proxy = proxy,
+				UseProxy = proxy != WebRequest.DefaultWebProxy
+			};
 
-                        return result;
-                    }
-                    else
-                    {
-                        string errorBody = $"Problem happened while contacting the Language API. " +
-                            $"Status code: {response.StatusCode}, Reason: {response.ReasonPhrase}";
-                        {
-                            var message = $"Problem happened during translation. " +
-                                            $"Status code: {response.StatusCode}, " +
-                                            $"Reason: {response.ReasonPhrase}";
 
-                            var exception = new LanguageException(message);
-                            exception.ParseResponseError(response);
-                            throw exception;
-                        }
-                    }
-                }
-            }
-        }
+			// In Core 2.1, we can use what is described in: https://docs.microsoft.com/en-gb/aspnet/core/fundamentals/http-requests?view=aspnetcore-2.1
+			// Otherwise, maybe use RestSharp?
+			_httpClient = new HttpClient(handler, true);
+			_httpClient.BaseAddress = new Uri(Constants.TranslatorApiBasePath);
+		}
 
-        /// <summary>
-        /// Gets the specified scope.
-        /// </summary>
-        /// <param name="settings">The settings.</param>
-        /// <returns></returns>
-        /// <remarks>
-        /// Default scope is ALL scope.
-        /// </remarks>
-        public ResponseBody Get(RequestHeader settings)
-        {
-            return GetAsync(settings, Scope.All).Result;
-        }
+		/// <summary>
+		/// Translates the content.
+		/// </summary>
+		/// <param name="content">The content. (Max.: 25 items)</param>
+		/// <param name="options">The options.</param>
+		/// <returns>
+		/// The translated content
+		/// </returns>
+		public IList<ResponseBody> Translate(IEnumerable<RequestContent> content, RequestParameter options)
+		{
+			return TranslateAsync(content, options).Result;
+		}
 
-        /// <summary>
-        /// Gets the specified scope.
-        /// </summary>
-        /// <param name="settings">The settings.</param>
-        /// <param name="scopes">The scopes.</param>
-        /// <returns></returns>
-        public ResponseBody Get(RequestHeader settings, Scope scopes)
-        {
-            return GetAsync(settings, scopes).Result;
-        }
-    }
+		/// <summary>
+		/// Translates the content.
+		/// </summary>
+		/// <param name="content">The content. (Max.: 25 items)</param>
+		/// <param name="options">The options.</param>
+		/// <returns>
+		/// The translated content
+		/// </returns>
+		public IList<ResponseBody> Translate(RequestContent content, RequestParameter options)
+		{
+			return TranslateAsync(new[] { content }, options).Result;
+		}
+
+		/// <summary>
+		/// Translates the content.
+		/// </summary>
+		/// <param name="content">The content. (Max.: 25 items)</param>
+		/// <param name="options">The options.</param>
+		/// <returns>
+		/// The translated content
+		/// </returns>
+		public async Task<IList<ResponseBody>> TranslateAsync(IEnumerable<RequestContent> content, RequestParameter options)
+		{
+			if (content.Count() > MaxNumberOfRequestContent)
+				throw new Exception($"Maximum amount of text to be translated have been reached (Max: {MaxNumberOfRequestContent})");
+			else if (string.Join(string.Empty, content).Length > MaxNumberOfCharacterPerRequest)
+				throw new Exception($"Maximum length of all the text to be translated is {MaxNumberOfCharacterPerRequest} characters.");
+
+			var qs = options.ToQueryString();
+
+			using (var request = new HttpRequestMessage(HttpMethod.Post, $"{UriExtensionPath}?{qs}"))
+			{
+				var requestBody = JsonConvert.SerializeObject(content.ToList());
+
+				// For the request, don't forget the request header stuff.
+				request.Content = new StringContent(requestBody, Encoding.UTF8, Constants.RequestMediaType);
+				// Todo We should be alternating between the subscription key. When we detect it's not working.
+				request.Headers.Add(Constants.RequestHeaderSubscriptionKey, _cognitiveServiceConfig.SubscriptionKey);
+
+				using (var response = await _httpClient.SendAsync(request).ConfigureAwait(false))
+				{
+					if (response.IsSuccessStatusCode)
+					{
+						var responseBody = await response.Content.ReadAsStringAsync();
+						var result = JsonConvert.DeserializeObject<List<ResponseBody>>(responseBody);
+						return result;
+					}
+					else
+					{
+						var message = $"Problem happened during translation. " +
+										$"Status code: {response.StatusCode}, " +
+										$"Reason: {response.ReasonPhrase}";
+
+						var exception = new TranslateException(message);
+						exception.ParseResponseError(response);
+						throw exception;
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Translates the content.
+		/// </summary>
+		/// <param name="content">The content. (Max.: 25 items)</param>
+		/// <param name="options">The options.</param>
+		/// <returns>
+		/// The translated content
+		/// </returns>
+		public Task<IList<ResponseBody>> TranslateAsync(RequestContent content, RequestParameter options)
+		{
+			return TranslateAsync(new[] { content }, options);
+		}
+
+
+		public void Dispose()
+		{
+			_httpClient?.Dispose();
+			_httpClient = null;
+		}
+
+	}
 }
